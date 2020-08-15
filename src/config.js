ed984c0a03ad68a93a9f3465e70a55a2eae9a44d
gmailbob <mianyangbob@gmail.com>
2020-08-15 01:48:30 -0400

    config aws-amplify. Amplify refers to Cognito as Auth, S3 as Storage, and API Gateway as API

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "zzy-notes-app-serverless-stack"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://zyzcjpdel0.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_B0AuJAMJD",
+    APP_CLIENT_ID: "3k9lvnnfr1nrsgll5idqtl4t3",
+    IDENTITY_POOL_ID: "us-east-1:2ce0a5ea-e4a6-43a8-a345-f6200b5798e1"
+  }
+};
